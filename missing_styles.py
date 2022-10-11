7d6e8f527c8a5fbcc3bb6142c7dc68aa68f48140
KarvikaKushwaha <108344229+KarvikaKushwaha@users.noreply.github.com>
2022-10-11 20:15:19 +0530

    Update missing_styles.py
    minor patch

--- missing_styles.py
+++ missing_styles.py
@@ -1,5 +1,5 @@
 import sys
-
+import logging
 from elasticsearch import Elasticsearch
 
 import pandas as pd
@@ -13,13 +13,13 @@ client = Elasticsearch(
     hosts=[{'host': "ec2-54-152-53-80.compute-1.amazonaws.com",
             'port': 9200}])
 
-df = pd.read_csv("data/data_files/" + sys.argv[1] + "/rawInput/new.csv")
+# df = pd.read_csv("data/data_files/" + sys.argv[1] + "/rawInput/new.csv")
+df = pd.read_csv("./input.csv")
 df = df[df["cp_tag"]=="Yes"]
 missing_list = list()
 style_present = dict()
 for i, row in df.iterrows():
     style_present[row['md5']] = True
-
 for i, row in df.iterrows():
     elasticquery={
         "query":{
@@ -32,6 +32,7 @@ for i, row in df.iterrows():
     try:
         family_id = result.get('hits').get('hits')[0].get('_source').get('familyid')
     except Exception as e:
+        logging.error("familyid not found for given md5")
         raise Exception("familyid not found for given md5: ", row['md5'], e)
         
     elasticquery={
@@ -45,17 +46,18 @@ for i, row in df.iterrows():
     response= client.search(index="similarity_etl_auto1",body=elasticquery)
 
     if len(response['hits']['hits']) > 0:
-        styles = response['hits']['hits'][0]['_source']['styles']
-        for i in range(len(styles)):
+        for i in range(len(response['hits']['hits'])):
+            hit = response['hits']['hits'][i]['_source']
             try:
-                if styles[i]['md5hash'] not in style_present:
-                    print(styles[i]['md5hash'], styles[i]['stylename'])
-                    missing_list.append([styles[i]['md5hash'],
+                if hit['md5hash'] not in style_present:
+                    print(hit['md5hash'], hit['stylename'])
+                    missing_list.append([hit['md5hash'],
                                     '=Image("https://render.myfonts.net/fonts/font_rend.php?id=' +
-                                    styles[i]['md5hash'] + \
+                                    hit['md5hash'] + \
                                     '&rt=The%20quick%20brown%20fox%20jumps%20over%20the%20lazy%20dog.&rs=200&fg=000000&bg=FFFFFF&w=3000")'])
             except:
                 pass
 df_missing = pd.DataFrame(missing_list, columns=['md5', 'image_url'])
 df_missing=df_missing.drop_duplicates(subset=['md5'], keep='first')
-df_missing.to_csv("data/data_files/"+sys.argv[1]+"/input/missing_styles.csv")
+# df_missing.to_csv("data/data_files/"+sys.argv[1]+"/input/missing_styles.csv")
+df_missing.to_csv("./output.csv")
